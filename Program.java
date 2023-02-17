e7b514b69c584753559f93e7b380829bc2e92d68
Francesca <119852988+PixelCat91@users.noreply.github.com>
2023-02-17 14:42:26 +0100

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,15 @@
+package es1;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ecco il nome dello studente: ");
+		
+		Studente s1 = new Studente();
+		
+		
+		System.out.println("Federico");
+	}
+
+}
---
+++ Studente.java
@@ -0,0 +1,11 @@
+package es1;
+
+public class Studente {
+
+	public String nome;
+	
+	public void stampaNome(String nome) {
+		
+		System.out.println(nome);
+	}
+}
