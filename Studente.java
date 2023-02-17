da40ea6bc3f350bfaf18894c6b1e3afc44458ccb
Exolab <Exolab@PC63.fritz.box>
2023-02-17 16:09:49 +0100

    messaggio federica syso3

--- Studente.java
+++ Studente.java
@@ -20,6 +20,9 @@ public class Studente {
 		public void studenteVoto(int voto) {
 			System.out.println("sei un fallito"+ voto);
 		}
+		public void studenteVoto2(int voto) {
+			System.out.println("sei bravo "+ voto);
+		}
 		
 		
 		System.out.println("Dario e Federico vogliono diventare development");
