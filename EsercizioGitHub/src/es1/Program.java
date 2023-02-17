9395da704f335c22ab69c9fa3766fd3c6cc9c7f2
Francesca <Francescadinisio@live.it>
2023-02-17 16:50:56 +0100

    hghghghghjgjhgjhgjj

--- EsercizioGitHub/src/es1/Program.java
+++ EsercizioGitHub/src/es1/Program.java
@@ -2,6 +2,9 @@ package es1;
 
 public class Program {
 
+	
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Ecco il nome dello studente: ");
--- EsercizioGitHub/src/es1/Studente.java
+++ EsercizioGitHub/src/es1/Studente.java
@@ -11,6 +11,7 @@ public class Studente {
 	
 	private void sysout() {
 		return; 
+		
 
 	}
 }
