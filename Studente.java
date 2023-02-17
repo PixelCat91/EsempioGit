7b43266ee6ae2b6c5cd19660178891615b5c29b4
Exolab <Exolab@PC63.fritz.box>
2023-02-17 15:39:49 +0100

    messaggio feedricaaaa

--- Studente.java
+++ Studente.java
@@ -4,9 +4,18 @@ public class Studente {
 
 	public String nome;
 	public String cognome;
+	public int eta ;
+	public String mat;
 	
 	public void stampaNome(String nome) {
 		
 		System.out.println(nome);
+		
+		
+		public void stampaNome2(String nome) {
+			
+			System.out.println(nome);
+			
+		
 	}
 }
